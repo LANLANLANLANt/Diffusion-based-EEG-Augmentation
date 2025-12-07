ddb17901b46ccc7e209f9126754a152fd271f734
LANLANLANLANt <159263179+LANLANLANLANt@users.noreply.github.com>
2025-12-08 00:48:01 +1100

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,1 +1,19 @@
+from torch.utils import data
+
+
+class EpochDataset(data.Dataset):
+    def __init__(self, epoches, targets: Optional[np.array], mode='train'):
+        self.epoches = epoches
+        self.targets = targets
+        self.mode = mode
+
+    def __len__(self):
+        return len(self.epoches)
+
+    def __getitem__(self, idx):
+        epoch = self.epoches[idx]
+        target = self.targets[idx]
+
+        return epoch, target
+
 
